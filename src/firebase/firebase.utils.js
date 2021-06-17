8a0098f8507b2ee5b3f2b6c11a01e1a77ea5c9c7
nahin91 <sec.tabrez03@gmail.com>
2021-06-17 14:19:44 +0200

    firebase util is added

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,18 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const config = {
+  apiKey: "AIzaSyDDCOyJeE4GqN_HcZopNIkqZQTGHlCZc0w",
+  authDomain: "my-ecommers-db.firebaseapp.com",
+  projectId: "my-ecommers-db",
+  storageBucket: "my-ecommers-db.appspot.com",
+  messagingSenderId: "126077555238",
+  appId: "1:126077555238:web:9e52be0959307b3bb24ce7",
+  measurementId: "G-B64K1RS3BF",
+};
+
+firebase.initializeApp(config)
+
+export const auth = firebase.auth()
+export const firestore = firebase.firestore()
