15fa3d2a8b668e63eed253584232c1b1e85289bb
nahin91 <sec.tabrez03@gmail.com>
2021-06-17 13:38:27 +0200

    signin page is added

--- src/App.js
+++ src/App.js
@@ -5,12 +5,7 @@ import { Switch, Route } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component.jsx";
 import Header from "./components/header/header.component";
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 
 function App() {
   return (
@@ -19,6 +14,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
---
+++ src/components/custom-button/custom-button.component.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+import './custom-button.component.scss'
+
+const CustomButton = ({children, ...otherProps}) =>(
+    <button className='custom-button' {...otherProps}>
+        {children}
+    </button>
+)
+
+export default CustomButton
